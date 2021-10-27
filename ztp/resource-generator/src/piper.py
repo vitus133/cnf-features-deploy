af86a99918670ae6109450d507352222bae118db
Vitaly Grinberg <vgrinber@redhat.com>
2021-10-27 20:52:40 +0300

    wip

--- ztp/resource-generator/src/piper.py
+++ ztp/resource-generator/src/piper.py
@@ -3,14 +3,13 @@ import yaml
 import fileinput
 import os
 import tempfile
-import time
-
+import shutil
 from watcher import PolicyGenWrapper
 
 
-def stdin_to_yamls():
+def files_to_yamls(files=None):
     yamls = []
-    with fileinput.input() as f:
+    with fileinput.input(files) as f:
         _, fn = tempfile.mkstemp(dir='/tmp')
         yamls.append(fn)
         inp_dump = open(fn, "w")
@@ -24,73 +23,24 @@ def stdin_to_yamls():
                 inp_dump = open(fn, "w")
     return(yamls)
         
-            
-# Configmap of a name starting with "source-crs" is converted to
-#   resources and placed into /tmp/ztp/source-crs
-# Other resources:
-#   If a Siteconfig / PGT resource has same name+ns as name+label:name 
-#   in a Namespace resource, they will be merged to a same file
-# Returns: dictionary of groupings
 
-def group_by_gvknns(yamls: list) -> dict:
-    grouping = {}
-    
-    def parse_cm(ym: dict):
-        name = ym.get('metadata').get('name')
-        if name.startswith("source-crs"):
-            os.mkdir("/tmp/ztp/source-crs")
-            data = ym.get('data')
-            for item in data:
-                with open(f"/tmp/ztp/source-crs/{item}", "w") as f:
-                    f.write(data[item])
-        else:
-            parse_other(ym)
-
-    def get_nns(ym: dict):
-        name = ym.get('metadata').get('name')
-        ns = ym.get('metadata').get('namespace')
-        return f"{name}_{ns}"
-    
-    def group(ym: dict, nns: str):
-        if grouping.get(nns) is None:
-            grouping[nns] = []
-        grouping[nns].append(ym)
-    
-    def parse_other(ym: dict):
-        nns = get_nns(ym)
-        group(ym, nns)
-
-    def parse_ns(ym: dict):
-        name = ym.get('metadata').get('name')
-        ns = ym.get('metadata').get('labels').get('name')
-        group(ym, f"{name}_{ns}")
-
-
+def make_files(yamls: dict):
+    os.mkdir("/tmp/manifests")
+    os.mkdir("/tmp/ztp/source-crs")
 
     for item in yamls:
         with open(item, "r") as ym:
             ym_dict = yaml.safe_load(ym)
-            os.unlink(item)
-            kind = ym_dict.get('kind')
-            handle = {
-                "ConfigMap": parse_cm,
-                "Namespace": parse_ns
-            }
-            if handle.get(kind) != None:
-                handle[kind](ym_dict)
+            if ym_dict.get('kind') == "Configmap" and ym.get('metadata').get('name').startswith("source-crs"):
+                data = ym.get('data')
+                for item in data:
+                    with open(f"/tmp/ztp/source-crs/{item}", "w") as f:
+                        f.write(data[item])
+                os.unlink(item)
             else:
-                parse_other(ym_dict)
-    return grouping
-
-
-def make_files(grouping: dict):
-    os.mkdir("/tmp/manifests")
-    for item in grouping:
-        with open(f"/tmp/manifests/{item}", "w") as f:
-            for n in range(len(grouping[item])):
-                yaml.dump(grouping[item][n], f, default_flow_style=False)
-                f.write("---\n")
+                shutil.move(f"/tmp/{item}", f"/tmp/manifests/{item}")
         
+
 def find_files(root):
         for d, dirs, files in os.walk(root):
             for f in files:
@@ -99,9 +49,8 @@ def find_files(root):
 
 if __name__ == "__main__":
     os.mkdir("/tmp/ztp")
-    yamls = stdin_to_yamls()
-    grouping = group_by_gvknns(yamls)
-    make_files(grouping)
+    yamls = files_to_yamls()
+    make_files(yamls)
     os.mkdir("/tmp/ztp/pg-out")
     dirs = ("/tmp/manifests", "/tmp/ztp/pg-out")
     PolicyGenWrapper(dirs)
