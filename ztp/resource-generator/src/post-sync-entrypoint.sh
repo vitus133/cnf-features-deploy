6640d4350d1226bab45f965eca48a11158aa8cc2
Vitaly Grinberg <vgrinber@redhat.com>
2022-03-07 09:59:08 +0200

    Add lock

--- ztp/resource-generator/src/post-sync-entrypoint.sh
+++ ztp/resource-generator/src/post-sync-entrypoint.sh
@@ -4,3 +4,7 @@ source $(dirname "$0")/common.sh
 init $1
 
 python watcher.py $(./get-pre-sync-rv.sh $1) $RESOURCE_NAME debug
+
+unlock_message=$(oc delete configmap/openshift-ztp-lock)
+unlock_result=$?
+"ztp-hooks.postsync $(date -R) INFO [post-sync-entrypoint] Sync unlock: $unlock_message, result $unlock_result 
