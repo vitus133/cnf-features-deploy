aa33de7bfc4daf9dcfc1a3de335a4025ce0a41d9
Vitaly Grinberg <vgrinber@redhat.com>
2022-03-08 11:51:54 +0200

    Fix kustomization

--- ztp/gitops-subscriptions/argocd/resource-hook-example/siteconfig/make-sites.py
+++ ztp/gitops-subscriptions/argocd/resource-hook-example/siteconfig/make-sites.py
@@ -8,9 +8,11 @@ num_sites = 99
 
 with open(f"{base_name}.yaml", "r") as t:
     template = list(yaml.safe_load_all(t))
+with open(f"kustomization.yaml", "r") as k:
+    kust = yaml.safe_load(k)
+kust["resources"] = kust["resources"][:3]
 
 for site in range(1, num_sites+1):
-    print(site)
     name = f"{base_name}-{site}"
     template[0]["metadata"]["name"] = name
     template[0]["metadata"]["labels"]["name"] = name
@@ -20,4 +22,9 @@ for site in range(1, num_sites+1):
     template[1]["spec"]["clusters"][0]["clusterLabels"]["sites"] = name
     with open(f"{name}.yaml", "w") as o:
         o.write(yaml.safe_dump_all(template, default_flow_style=False))
+    kust["resources"].append(f"{name}.yaml")
+
+with open(f"kustomization.yaml", "w") as k:
+    k.write(yaml.safe_dump(kust, default_flow_style=False))
+    
 
