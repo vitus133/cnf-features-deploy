f6d0ddedfd96fb5415349f4b11850418db13b93b
Vitaly Grinberg <vgrinber@redhat.com>
2022-03-08 12:06:06 +0200

    Add sleep after post-sync so I can rsh to the pod

--- ztp/resource-generator/src/post-sync-entrypoint.sh
+++ ztp/resource-generator/src/post-sync-entrypoint.sh
@@ -8,3 +8,6 @@ python watcher.py $(./get-pre-sync-rv.sh $1) $RESOURCE_NAME debug
 unlock_message=$(oc delete configmap/openshift-ztp-lock)
 unlock_result=$?
 echo "ztp-hooks.postsync $(date -R) INFO [post-sync-entrypoint] Sync unlock: $unlock_message, result $unlock_result" >> /proc/1/fd/1
+
+# for debug on the pod
+sleep inf
